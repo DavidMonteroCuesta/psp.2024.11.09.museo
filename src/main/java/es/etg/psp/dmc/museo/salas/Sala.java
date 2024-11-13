960d21b2a0f1204e19a3c66397e6f9e1483f6ce2
monterodc17 <monterodc17@gmail.com>
2024-11-13 12:05:04 +0100

    museo

--- src/main/java/es/etg/psp/dmc/museo/salas/Sala.java
+++ src/main/java/es/etg/psp/dmc/museo/salas/Sala.java
@@ -18,6 +18,7 @@ public class Sala {
 
         if (entra) visitantes++;
         else visitantes--;
+        System.out.println(Thread.currentThread().getName());
         Sala.class.notify();
     }    
 }
